d488f90a31bdb58d96b7cabc85458b1f0adf314f
Tomasz Buszewski <tomek.buszewski@gmail.com>
2026-04-14 12:18:55 +0200

    feat(SKD-5): tanstack router added (#2)

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,11 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { routeTree } from "./routeTree.gen";
+
 import "@skeddo/ui/src/style.css";
-import { Button } from "@skeddo/ui/atoms/button";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({ routeTree });
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Button>btn</Button>
-    <div className="bg-red-100 text-2xl">hello</div>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vitest/config'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from "@tailwindcss/vite";
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
   test: {
