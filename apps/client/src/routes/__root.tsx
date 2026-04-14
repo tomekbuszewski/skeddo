d20269ee372aac83ccb05581cf7918dec47d9552
Tomek Buszewski <tomek.buszewski@gmail.com>
2026-04-14 12:18:22 +0200

    feat(SKD-5): tanstack router added

---
+++ apps/client/src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <React.Fragment>
+      <div>Hello "__root"!</div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </React.Fragment>
+  );
+}
